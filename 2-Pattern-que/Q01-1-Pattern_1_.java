ad02afdfd2dfb1bc82c24a8bd451c4f8c9bf807a
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-04-10 08:10:27 +0530

    pattren_Problems

---
+++ 2-Pattern-que/Q01-1-Pattern_1_.java
@@ -0,0 +1,14 @@
+//
+//  for n=5
+//  * * * * * 
+//
+
+
+public class Q1-Pattern_1_{
+    public static void main(String[] args) {
+        int n=5;
+        for (int i = 1; i <= n; i++) {
+            System.out.print("* ");
+        }
+    }
+}
