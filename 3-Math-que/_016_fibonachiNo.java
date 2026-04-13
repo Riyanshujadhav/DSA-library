d12012010b235a92a4fb99f54667cea7716c9663
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-04-13 22:28:37 +0530

    fibonachi

--- 3-Math-que/_016_fibonachiNo.java
+++ 3-Math-que/_016_fibonachiNo.java
@@ -1,8 +1,6 @@
-
 import java.util.Scanner;
 
 public class _016_fibonachiNo {
-
     public static int fibo(int n){
         if(n==0||n==1){
             return n;
@@ -10,7 +8,6 @@ public class _016_fibonachiNo {
             return (fibo(n-2)+fibo(n-1));
         }
     }
-
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         int n=sc.nextInt();
