dcb2087db56cae51fd9326d1d4df6cad884a186a
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-04-13 22:29:50 +0530

    pascalTriangle

--- 3-Math-que/_021_pascalTriangle.java
+++ 3-Math-que/_021_pascalTriangle.java
@@ -1,10 +1,7 @@
-
 import java.util.Scanner;
-
 public class _021_pascalTriangle {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        
+        Scanner sc=new Scanner(System.in);     
         int n=sc.nextInt();
         for(int i=0;i<n;i++){
             int val=1;
