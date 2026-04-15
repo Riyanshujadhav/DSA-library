c7fb401f667843d3945848d40c50e8a8ac9e7a7f
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-04-15 19:36:08 +0530

    Assigment-3-Math

--- 3-Math-que/_033_09que.java
+++ 3-Math-que/_033_09que.java
@@ -1,7 +1,6 @@
 //Take as input a number N, print "Prime" if it is prime if not Print "Not Prime".
 import java.util.*;
 public class _033_09que {
-
     public static boolean primecheck(int n){
         for(int i=2;i<=(int)Math.sqrt(n);i++){
             if(n%i==0){
@@ -10,8 +9,6 @@ public class _033_09que {
         }
         return true;
     }
-
-
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
